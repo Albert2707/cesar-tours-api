150bd3880299f570a9866745d13cdf64513f9afa
Albert <albertjohan2707@gmail.com>
2024-11-16 12:45:20 -0400

    initializing typeorm

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express, { Express, json } from 'express'
 import { config } from "dotenv"
 import { emailRouter } from './routes/email.route';
 import cors from 'cors'
+import { dataSource } from './ormconfig';
+import "reflect-metadata";
 config();
 const app: Express = express();
 const allowedOrigins = ['http://localhost:5173'];
@@ -12,6 +14,12 @@ const options: cors.CorsOptions = {
 app.use(cors(options))
 app.use(json())
 app.use("/api/email", emailRouter)
-app.listen(process.env.PORT || 3000, () => {
-    console.log("Server is runnnig on port:" + process.env.PORT || 3000)
+dataSource.initialize().then(async () => {
+    app.listen(process.env.PORT || 3000, () => {
+        console.log("Server is runnnig on port:" + process.env.PORT || 3000)
+    })
+    console.log("Data Source has been initialized!");
+}).catch(err => {
+    console.log(err.message)
+    throw new Error(err.message)
 })
