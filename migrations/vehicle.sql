8a13f0456db7853c2143434965c6448ed9835f13
Albert2707 <albertjohan2707@gmail.com>
2024-11-17 17:48:34 -0400

    migrations script

---
+++ migrations/vehicle.sql
@@ -0,0 +1,4 @@
+insert into vehicles (make, model, capacity, luggage_capacity, price_per_km, img_url, status) 
+values ('Chevrolet', 'Suburban', 8, 8, 30, 'https://i.ibb.co/cDFkTtw/suburban.png', 1),
+ ('Toyota', 'Coaster', 30, 15, 50, 'https://i.ibb.co/JrwjWgZ/coaster.png', 1),
+  ('Toyota', 'Hiace', 15, 10, 50, 'https://i.ibb.co/wMy6KND/hiace.png', 1);
