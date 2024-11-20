e18f3a66a6f1ee1daabf8cffc8a2e1d7949b035a
Albert <albertjohan2707@gmail.com>
2024-11-19 23:03:19 -0400

    method, to handle token and password

---
+++ src/helpers/encrypt.ts
@@ -0,0 +1,19 @@
+import * as dotenv from "dotenv"
+import * as jwt from "jsonwebtoken"
+import * as bcrypt from "bcrypt"
+import { payload } from "../dto/user.dto"
+dotenv.config()
+
+const { JWT_SECRET = "" } = process.env
+export class Encrypt {
+    static async encryptpass(password: string) {
+        return bcrypt.hash(password, 12);
+    }
+    static comparepassword(hashPassword: string, password: string) {
+        return bcrypt.compare(password, hashPassword)
+    }
+    static generateToken(payload: payload) {
+        return jwt.sign(payload, JWT_SECRET, { expiresIn: '1d' })
+    }
+
+}
