20c8e7c20b135d37a7561344820f535bd66907fa
Albert <albertjohan2707@gmail.com>
2024-11-19 23:01:19 -0400

    migrations

--- migrations/vehicle.sql
+++ migrations/vehicle.sql
@@ -1,4 +1,8 @@
+use cesar_tours
 insert into vehicles (make, model, capacity, luggage_capacity, price_per_km, img_url, status) 
 values ('Chevrolet', 'Suburban', 8, 8, 30, 'https://i.ibb.co/cDFkTtw/suburban.png', 1),
  ('Toyota', 'Coaster', 30, 15, 50, 'https://i.ibb.co/JrwjWgZ/coaster.png', 1),
   ('Toyota', 'Hiace', 15, 10, 50, 'https://i.ibb.co/wMy6KND/hiace.png', 1);
+  select * from vehicles
+  
+  update vehicles set make ='Tahoe' where id = 1
