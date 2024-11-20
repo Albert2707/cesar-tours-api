74242e21ca173e709091ebd1d306b6e936b859f7
Albert <albertjohan2707@gmail.com>
2024-11-19 23:03:31 -0400

    user entity

---
+++ src/entity/User.entity.ts
@@ -0,0 +1,26 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity({ name: "user", schema: "cesar_tours" })
+
+export class User {
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column({ unique: true })
+    email: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    password: string;
+
+    @CreateDateColumn({ name: "created_at" })
+    createAt: Date;
+  
+    @UpdateDateColumn({ name: "updated_at" })
+    updateAt: Date;
+
+    @Column()
+    role: string;
+}
