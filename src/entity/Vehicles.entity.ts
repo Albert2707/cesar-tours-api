eef0bf49356b86d9e546b1acdfba6f313288ceb6
Albert <albertjohan2707@gmail.com>
2024-12-01 23:06:59 -0400

    fix: adding new columns to vehicle entity

--- src/entity/Vehicles.entity.ts
+++ src/entity/Vehicles.entity.ts
@@ -1,21 +1,34 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 @Entity({ name: "vehicles", schema: "cesar_tours" })
 export class Vehicle {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
   @Column()
-  make: string;
+  brand: string;
+
   @Column()
   model: string;
+
   @Column()
   capacity: number;
+
   @Column()
   luggage_capacity: number;
-  @Column({type:"decimal", precision:7, scale:2})
-  price_per_km: string;
+
+  @Column({ type: "decimal", precision: 7, scale: 2 })
+  price_per_km: number;
+
   @Column()
   img_url: string;
+
   @Column()
   status: boolean;
+
+  @CreateDateColumn({ name: "created_at" })
+  createAt: Date;
+
+  @UpdateDateColumn({ name: "updated_at" })
+  updateAt: Date;
 }
