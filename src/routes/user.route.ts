5a7eb588b96bee76e88cad6b7d4b8cff4520011c
Albert <albertjohan2707@gmail.com>
2024-12-01 23:08:31 -0400

    feat: user router

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,7 +1,9 @@
 import { Router } from "express"
 import { AuthController } from "../controllers/auth.controller";
+import { authorization } from "../middlewares/authorization";
 const userRouter = Router();
 userRouter.post("/login", AuthController.login)
-userRouter.post("/register", AuthController.register)
-userRouter.post("/logout", AuthController.logout)
+userRouter.post("/register",authorization, AuthController.register)
+userRouter.post("/logout", authorization,AuthController.logout)
+userRouter.get("/profile",authorization ,AuthController.getProfile)
 export { userRouter }
