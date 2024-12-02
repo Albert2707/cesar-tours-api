d6d763fc3a42516f75de8e8e26bb61aac8b4ce74
Albert <albertjohan2707@gmail.com>
2024-12-01 23:05:44 -0400

    feat: create new vehicle controller

--- src/controllers/vehicles.controller.ts
+++ src/controllers/vehicles.controller.ts
@@ -1,7 +1,8 @@
 import { NextFunction, Request, Response } from "express";
-import { Vehicle } from "../entity/Vehicles.entity";
 import { dataSource } from "../config/ormconfig";
 import { MoreThanOrEqual } from "typeorm";
+import { Vehicle } from "../entity/Vehicles.entity";
+import { joiSchemaCreateVehicle } from "../helpers/validateBody";
 
 export class VehiclesController {
   static getVehicles = async (req: any, res: Response): Promise<any> => {
@@ -24,4 +25,26 @@ export class VehiclesController {
         return res.status(500).json({ message: err.message });
     }
   };
+
+  static async createVehicle(req: Request, res: Response): Promise<any> {
+    try {
+      const { brand, model, capacity, luggage_capacity, price_per_km, img_url, status } = req.body
+      const source = dataSource.getRepository(Vehicle);
+      const { error } = joiSchemaCreateVehicle.validate(req.body)
+      if (error) return res.status(400).json({ message: error.details[0].message })
+      const newVehicle = source.create({
+        brand,
+        model,
+        capacity,
+        luggage_capacity,
+        price_per_km,
+        img_url,
+        status
+      })
+      await source.save(newVehicle)
+      return res.status(201).json({ message: "Vehicle created successfully" })
+    } catch (error) {
+      if (error instanceof Error) return res.status(500).json({ message: error.message })
+    }
+  }
 }
