5d646b89ab1c6733194f2601c7a637459c47a465
Albert <albertjohan2707@gmail.com>
2024-12-01 23:08:44 -0400

    feat: vehicle router

--- src/routes/vehicle.route.ts
+++ src/routes/vehicle.route.ts
@@ -1,7 +1,9 @@
 import * as express from "express";
 import { VehiclesController } from "../controllers/vehicles.controller";
+import { authorization } from "../middlewares/authorization";
 
 
 const router = express.Router();
-router.get("/getVehicles/:capacity/:luggage_capacity",VehiclesController.getVehicles);
-export  {router as vehicleRouter};
+router.get("/getVehicles/:capacity/:luggage_capacity", VehiclesController.getVehicles);
+router.post("/createVehicle", authorization, VehiclesController.createVehicle);
+export { router as vehicleRouter };
