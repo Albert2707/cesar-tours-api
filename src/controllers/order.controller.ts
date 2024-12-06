c41f61ff861afcb84aafb05da34405833a38d682
Albert <albertjohan2707@gmail.com>
2024-12-06 12:25:37 -0400

    fix: orders pagination

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -1,18 +1,20 @@
 import { Request, Response } from "express";
 import { dataSource } from "../config/ormconfig"
 import { Order } from "../entity/Order.entity"
-
+import { generateCustomOrderNum } from "../helpers/uuid";
+import { Customer } from "../entity/Customer.entity";
 export class OrderController {
     static getOrders = async (req: Request, res: Response): Promise<any> => {
         try {
-            const { skip = 0, limit = 5, status } = req.query;
+            const { skip = 1, limit = 5, status } = req.query;  // Página 1 por defecto
             let whereClause: any = {};
 
             if (status && status !== 'all') {
                 whereClause.status = Number(status);
             }
-            const skipValue = parseInt(skip as string, 10) || 0;
-            const limitValue = parseInt(limit as string, 10) || 5;
+            let skipValue = (parseInt(skip as string, 10) - 1) * parseInt(limit as string, 10);  // Ajustamos skip
+            let limitValue = parseInt(limit as string, 10) || 5;
+
             const [order, total] = await dataSource.getRepository(Order).findAndCount({
                 relations: ["customer", "vehicle", "country"],
                 skip: skipValue,
@@ -22,12 +24,42 @@ export class OrderController {
                     "createAt": "DESC"
                 }
             });
-            const currentPage =  Math.floor(skipValue / limitValue) + 1  
+
+            const currentPage = Math.floor(skipValue / limitValue) + 1;
             const totalPages = Math.ceil(total / limitValue);
-            return res.status(200).json({ order, total, totalPages, currentPage, hasNextPage: currentPage < totalPages });
+
+            return res.status(200).json({
+                order,
+                total,
+                totalPages,
+                currentPage,
+                hasNextPage: currentPage < totalPages
+            });
         } catch (error) {
-            console.error(error)
+            console.error(error);
             if (error instanceof Error) return res.status(500).json({ message: error.message });
+        }
+    };
+
+
+    static postOrder = async (req: Request, res: Response): Promise<any> => {
+        try {
+            const order = dataSource.getRepository(Order);
+            // const customer = dataSource.getRepository(Customer);
+            // const { name, lastName, email, phone, optionalPhone, countryId } = req.body;
+            // const newCustomer = customer.create({ name, lastName, email, phone, optionalPhone, countryId });
+            // const customerCreated = await customer.save(newCustomer);
+            // console.log(customerCreated);
+            const body = req.body as Order;
+            const orderToCreate = { ...body, order_num: generateCustomOrderNum() }
+            const findOrder = await order.findOne({ where: { order_num: orderToCreate.order_num } })
+            if (findOrder) return res.status(400).json({ message: "Order already exists" })
+            const newOrder = order.create(orderToCreate);
+            const orderCreated = await order.save(newOrder);
+            return res.status(201).json({ message: "Order created successfully", orderCreated });
+            // return res.status(201).json({ message: "Order created successfully" });
+        } catch (error) {
+            if (error instanceof Error) return res.status(500).json({ message: error.message })
         }
     }
 }
