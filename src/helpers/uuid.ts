1d442027e6de09168d1da45c5d902e6e30d80aca
Albert <albertjohan2707@gmail.com>
2024-12-06 16:58:09 -0400

    fix: shorter uuid

--- src/helpers/uuid.ts
+++ src/helpers/uuid.ts
@@ -2,6 +2,6 @@ import { v4 as uuidv4 } from 'uuid';
 
 export const generateCustomOrderNum = () => {
     const seed = Date.now();  
-    const customOrderNum = `${seed}${uuidv4().replace(/-/g, "")}`.substring(0, 14);
+    const customOrderNum = `${seed}${uuidv4().replace(/-/g, "")}`.substring(0, 12);
     return customOrderNum;
 };
