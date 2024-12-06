b18a5921c45f0df8050a150a42f43070a0fc9b3d
Albert <albertjohan2707@gmail.com>
2024-12-06 12:24:19 -0400

    feat: uuid generator function

---
+++ src/helpers/uuid.ts
@@ -0,0 +1,7 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export const generateCustomOrderNum = () => {
+    const seed = Date.now();  
+    const customOrderNum = `${seed}${uuidv4().replace(/-/g, "")}`.substring(0, 14);
+    return customOrderNum;
+};
