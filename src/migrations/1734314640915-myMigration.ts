22461a6808e96e5aaefd8beabf0677c972d743db
Albert <albertjohan2707@gmail.com>
2024-12-16 12:47:55 -0400

    feat: adding more countries to the migration

--- src/migrations/1734314640915-myMigration.ts
+++ src/migrations/1734314640915-myMigration.ts
@@ -1,26 +1,54 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class MyMigration1734314640915 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+        INSERT INTO cesar_tours.country (country_id, country) VALUES
+        ('DO', 'Dominican Republic'),
+        ('US', 'United States'),
+        ('CA', 'Canada'),
+        ('MX', 'Mexico'),
+        ('BR', 'Brazil'),
+        ('AR', 'Argentina'),
+        ('DE', 'Germany'),
+        ('FR', 'France'),
+        ('JP', 'Japan'),
+        ('AU', 'Australia'),
+        ('GB', 'United Kingdom'),
+        ('IT', 'Italy'),
+        ('ES', 'Spain'),
+        ('IN', 'India'),
+        ('CN', 'China'),
+        ('RU', 'Russia'),
+        ('ZA', 'South Africa'),
+        ('NG', 'Nigeria'),
+        ('EG', 'Egypt'),
+        ('KR', 'South Korea'),
+        ('SE', 'Sweden'),
+        ('NO', 'Norway'),
+        ('FI', 'Finland'),
+        ('NL', 'Netherlands'),
+        ('PL', 'Poland'),
+        ('CH', 'Switzerland'),
+        ('BE', 'Belgium'),
+        ('AT', 'Austria'),
+        ('SG', 'Singapore'),
+        ('TH', 'Thailand'),
+        ('PH', 'Philippines'),
+        ('ID', 'Indonesia'),
+        ('MY', 'Malaysia'),
+        ('PK', 'Pakistan'),
+        ('UA', 'Ukraine'),
+        ('KE', 'Kenya'),
+        ('CL', 'Chile'),
+        ('PE', 'Peru'),
+        ('CO', 'Colombia'),
+        ('VE', 'Venezuela')
+      `);
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
-            INSERT INTO cesar_tours.country (country_id, country) VALUES
-            ('DO', 'Dominican Republic'),
-            ('US', 'United States'),
-            ('CA', 'Canada'),
-            ('MX', 'Mexico'),
-            ('BR', 'Brazil'),
-            ('AR', 'Argentina'),
-            ('DE', 'Germany'),
-            ('FR', 'France'),
-            ('JP', 'Japan'),
-            ('AU', 'Australia')
-          `);
-          
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
             DELETE FROM cesar_tours.country 
             WHERE country IN (
               'Dominican Republic',
@@ -35,6 +63,5 @@ export class MyMigration1734314640915 implements MigrationInterface {
               'Australia'
             )
           `);
-    }
-
+  }
 }
