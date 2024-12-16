e09df49f594ca7b7813ecb37a462f350f0200af6
Albert <albertjohan2707@gmail.com>
2024-12-16 08:29:37 -0400

    feat: typeorm migrations

--- src/migrations/1734314640915-myMigration.ts
+++ src/migrations/1734314640915-myMigration.ts
@@ -1,21 +1,22 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class NuevaMigracion1733403824609 implements MigrationInterface {
+export class MyMigration1734314640915 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
             INSERT INTO cesar_tours.country (country_id, country) VALUES
-            (uuid_generate_v4(), 'Dominican Republic'),
-            (uuid_generate_v4(), 'United States'),
-            (uuid_generate_v4(), 'Canada'),
-            (uuid_generate_v4(), 'Mexico'),
-            (uuid_generate_v4(), 'Brazil'),
-            (uuid_generate_v4(), 'Argentina'),
-            (uuid_generate_v4(), 'Germany'),
-            (uuid_generate_v4(), 'France'),
-            (uuid_generate_v4(), 'Japan'),
-            (uuid_generate_v4(), 'Australia')
+            ('DO', 'Dominican Republic'),
+            ('US', 'United States'),
+            ('CA', 'Canada'),
+            ('MX', 'Mexico'),
+            ('BR', 'Brazil'),
+            ('AR', 'Argentina'),
+            ('DE', 'Germany'),
+            ('FR', 'France'),
+            ('JP', 'Japan'),
+            ('AU', 'Australia')
           `);
+          
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
