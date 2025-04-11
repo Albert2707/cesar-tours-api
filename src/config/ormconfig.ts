3d3bc9786245dd9c17183ed80191feb964297e1f
AlbertDev <albertjohan2707@gmail.com>
2025-04-11 18:17:46 -0400

    fix: correct synchronize condition in data source configuration

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -6,7 +6,7 @@ export const dataSource = new DataSource({
     type: "mysql",
     url:MYSQL_PUBLIC_URL,
     logger: 'file',
-    synchronize: NODE_ENV === "dev"|| NODE_ENV === 'test' ? true : false,
+    synchronize: NODE_ENV === "dev"? true : false,
     logging: NODE_ENV === "dev"|| NODE_ENV === 'test' ? false : false,
     entities: ["src/entity/**/*.ts"],
     migrations: ["src/migrations/*.ts"],
