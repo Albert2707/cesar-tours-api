a5abb04ecb2075a3a732696864886f01145552ef
AlbertDev <albertjohan2707@gmail.com>
2025-04-11 18:04:20 -0400

    fix: improve vehicle test setup and enhance test descriptions

--- tests/Vehicles.test.ts
+++ tests/Vehicles.test.ts
@@ -1,16 +1,30 @@
 import request from "supertest";
 import app from "../src/index";
 import { dataSource } from "../src/config/ormconfig";
+import { Vehicle } from "../src/entity/Vehicles.entity";
 
 describe("Vehicles", () => {
   beforeAll(async () => {
     if (!dataSource.isInitialized) {
       await dataSource.initialize();
     }
+
+    // Inserta un vehículo de prueba en la base de datos
+    const vehicleRepository = dataSource.getRepository(Vehicle);
+    const testVehicle = vehicleRepository.create({
+      brand: "Toyota",
+      model: "Corolla",
+      capacity: 5,
+      luggage_capacity: 3,
+      price_per_km: 0.75,
+      img_url: "https://example.com/image.jpg",
+      status: true,
+    });
+    await vehicleRepository.save(testVehicle);
   });
 
   afterAll(async () => {
-    await dataSource.destroy(); // cierra la conexión
+    await dataSource.destroy(); // Cierra la conexión
   });
 
   it("should return a list of vehicles", async () => {
@@ -21,13 +35,13 @@ describe("Vehicles", () => {
     expect(res.body.length).toBeGreaterThan(0);
   });
 
-  it("should return 404", async () => {
+  it("should return 404 for an invalid route", async () => {
     const res = await request(app).get("/api/vehicle/getVehicles/taho");
 
     expect(res.status).toBe(404);
   });
 
-  it("should return a list of vehicles filter by capacity en luggage", async () => {
+  it("should return a list of vehicles filtered by capacity and luggage", async () => {
     const res = await request(app).get(
       "/api/vehicle/getVehicles?capacity=1&luggage_capacity=1"
     );
