bf1dbfc1385b3f02429795be222903c46a2598e3
BlackMamba2707 <168677550+BlackMamba2707@users.noreply.github.com>
2025-08-31 00:16:18 -0400

    fix: refactor database configuration to use individual environment variables

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -1,10 +1,14 @@
 import { DataSource } from "typeorm";
 import { config } from "dotenv";
 config();
-const {MYSQL_PUBLIC_URL,NODE_ENV} = process.env;
+const {NODE_ENV,MYSQL_USER,MYSQL_PASSWORD,MYSQL_HOST,MYSQL_PORT,MYSQL_DATABASE} = process.env;
 export const dataSource = new DataSource({
     type: "mysql",
-    url:MYSQL_PUBLIC_URL,
+    host:MYSQL_HOST,
+    username:MYSQL_USER,
+    password:MYSQL_PASSWORD,
+    port: +MYSQL_PORT!,
+    database:MYSQL_DATABASE,
     logger: 'file',
     synchronize: NODE_ENV === "dev"? true : false,
     logging: NODE_ENV === "dev"|| NODE_ENV === 'test' ? false : false,
